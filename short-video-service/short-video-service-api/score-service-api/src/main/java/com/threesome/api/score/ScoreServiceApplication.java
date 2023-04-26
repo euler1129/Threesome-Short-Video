965c0611f642aba1ae34f2ee572c628a88bc1604
euler1129 <896498334@qq.com>
2023-04-26 15:27:19 +0800

    1.parent project 1.1 Modified pom.xml 2.dao module 2.1 Modified Startup class 2.2 Modified pom.xml 3.common module 3.1 Modified Startup class 3.2 Modified pom.xml 4.api module 4.1 Removed UserCenter, MemberCenter, ScoreCenter, MediaCenter and PaymentCenter modules 4.2 Added user-service-api, member-service-api, score-service-api, media-service-api and payment-service-api modules 4.3 Modified 'API List.txt' file 4.4 Modified pom.xml

---
+++ short-video-service/short-video-service-api/score-service-api/src/main/java/com/threesome/api/score/ScoreServiceApplication.java
@@ -0,0 +1,11 @@
+package com.threesome.api.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreServiceApplication.class, args);
+    }
+}
