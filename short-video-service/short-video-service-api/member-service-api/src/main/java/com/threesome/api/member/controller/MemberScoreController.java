accb0db6c389bd4bb916fa99186793023edad67f
euler1129 <896498334@qq.com>
2023-04-26 22:13:52 +0800

    1.Added data source config for each api modules 2.Modified DaoApplication.java 3.Modified MemberScoreController.java

--- short-video-service/short-video-service-api/member-service-api/src/main/java/com/threesome/api/member/controller/MemberScoreController.java
+++ short-video-service/short-video-service-api/member-service-api/src/main/java/com/threesome/api/member/controller/MemberScoreController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberScoreController {
-    @GetMapping("/member/level")
+    @GetMapping("/member/score")
     public void retrieve(){
 
     }
--- short-video-service/short-video-service-dao/src/main/java/com/threesome/dao/DaoApplication.java
+++ short-video-service/short-video-service-dao/src/main/java/com/threesome/dao/DaoApplication.java
@@ -3,7 +3,7 @@ package com.threesome.dao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DaoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DaoApplication.class, args);
